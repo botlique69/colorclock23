88432767049c4884438394b3472e0a179d146caf
que <118755385+botlique69@users.noreply.github.com>
2023-12-23 14:55:51 +0100

    változás

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+ function colorclock(){
 var date = new Date();
 var hours = date.getHours();
 var minutes = date.getMinutes();
 var seconds = date.getSeconds();
+
+if (hours <= 9) {
+    hours = '0' + hours;
+}
+if (minutes <= 9) {
+    minutes = '0' + minutes;
+}
+if (seconds <= 9) {
+    seconds = '0' + seconds;
+}
+
 var clockFace = hours + ':' + minutes + ':' + seconds;
-document.getElementById('color-clock').innerHTML = clockFace;
+document.getElementById('clock').innerHTML = clockFace;
 console.log(clockFace);
+}
+colorClock();
