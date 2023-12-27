f202c0f6d4af47eb35223bb3300684a55b07fd82
que <118755385+botlique69@users.noreply.github.com>
2023-12-27 06:41:55 +0100

    script a bodyra

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
- function colorclock(){
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
 
-if (hours <= 9) {
-    hours = '0' + hours;
-}
-if (minutes <= 9) {
-    minutes = '0' + minutes;
-}
-if (seconds <= 9) {
-    seconds = '0' + seconds;
+colorClock();
+
+function colorClock() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if (hours <= 9) {
+        hours = '0' + hours;
+    }
+    if (minutes <= 9) {
+        minutes = '0' + minutes;
+    }
+    if (seconds <= 9) {
+        seconds = '0' + seconds;
+    }
+
+    var clockFace = hours + ':' + minutes + ':' + seconds;
+    var hexColor = '#' + hours + minutes + seconds;
+    document.getElementById('clock').innerHTML = clockFace;
+    document.body.style.background = hexColor;
+    setTimeout(function () {
+        colorClock();
+    }, 1000);
 }
 
-var clockFace = hours + ':' + minutes + ':' + seconds;
-document.getElementById('clock').innerHTML = clockFace;
-console.log(clockFace);
-}
-colorclock();
+colorClock();
+
